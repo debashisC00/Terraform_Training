52363dcfc2d60f28568338cc70fa485b8fcef947
debashisC00 <100515208+debashisC00@users.noreply.github.com>
2022-04-06 15:12:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "debashis-vpc" {
-cidr_block = "21.22.0.0/16"
-instance_tenancy = "default"
-tags = {
-Name="debashis-vpc"
+        cidr_block = "21.22.0.0/16"
+        instance_tenancy = "default"
+        tags = {
+        Name="debashis-vpc"
 }
 }
 
 resource "aws_internet_gateway" "debashis-gateway" {
-vpc_id = "${aws_vpc.debashis-vpc.id}"
-tags = {
-Name="debashis-gateway"
+        vpc_id = "${aws_vpc.debashis-vpc.id}"
+        tags = {
+        Name="debashis-gateway"
 }
 }
 
